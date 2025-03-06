00aab8acf08f02d8ebc35db5aed2504762de1b8d
Richard Abishai <rickoshade1891@gmail.com>
2025-03-06 18:44:34 +0530

    Add Header component and Button component with Radix UI integration; update SiteLayout for authentication handling

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,11 +1,21 @@
+import Header from "@/components/Header";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+
 export default function SiteLayout({
     children,
   }: {
     children: React.ReactNode;
   }) {
     return (
-      <div className="flex min-h-screen justify-center items-center">
-          {children}
+      <div className="flex flex-col h-screen min-h-screen bg-gray-50">
+
+        <SignedIn>{/* AuthHeader */}</SignedIn>
+
+        <SignedOut>
+            <Header/>
+        </SignedOut>
+
+        <main className="flex-1 w-full p-4 max-w-6xl mx-auto">{children}</main>
       </div>
     );
   }
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='p-4 flex items-center justify-between text-sm font-medium max-w-6xl mx-auto w-full'>
+        <h1 className='text-2xl lg:text-4xl font-serif font-bold'>
+            Scribe
+        </h1>
+    </header>
+  )
+}
