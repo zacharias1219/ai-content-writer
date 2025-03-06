1767585467f60d763649f185fefb62adb5e637ef
Richard Abishai <rickoshade1891@gmail.com>
2025-03-06 18:49:55 +0530

    Enhance Header component with Clerk authentication buttons and loading state; add navigation links for improved user experience

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,33 @@
+import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton } from '@clerk/nextjs'
+import { Loader2Icon } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
 
 export default function Header() {
   return (
     <header className='p-4 flex items-center justify-between text-sm font-medium max-w-6xl mx-auto w-full'>
-        <h1 className='text-2xl lg:text-4xl font-serif font-bold'>
-            Scribe
-        </h1>
+        <Link href='/'>
+            <h1 className='text-2xl lg:text-4xl font-serif font-bold'>
+                Scribe
+            </h1>
+        </Link>
+
+        <div className='flex items-center gap-8'>
+            <Link href='/new-post'>
+                Write
+            </Link>
+            <ClerkLoading>
+                <Loader2Icon/>
+            </ClerkLoading>
+
+            <ClerkLoaded>
+                <SignInButton/>
+                <Button>
+                    <SignUpButton/>
+                </Button>
+            </ClerkLoaded>
+        </div>
     </header>
   )
 }
