9a20956a526a777a7cbb05d558a054d50296ed5d
Richard Abishai <rickoshade1891@gmail.com>
2025-03-07 18:06:40 +0530

    Refactor LandingPage component to enhance layout and styling for improved user experience

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export default function LandingPage() {
+const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className='h-full flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-28 lg:gap-0'>
+        <div className='space-y-10 md:space-y-12'>
+            <h1 className='text-6xl lg:text-9xl text-zinc-800 font-serif font-medium'>
+                Where Stories
+                <br/>
+                Spark Ideas
+            </h1>
+            <p></p>
+        </div>
+    </div>
   )
 }
+
+export default LandingPage;
