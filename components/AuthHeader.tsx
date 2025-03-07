aa17a206c88790232b4a2919fd944b75c51e6027
Richard Abishai <rickoshade1891@gmail.com>
2025-03-07 17:56:17 +0530

    Enhance AuthHeader component with search and new post links; integrate loading and user button states for improved functionality

--- components/AuthHeader.tsx
+++ components/AuthHeader.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link"
+import { SearchIcon, EditIcon, Loader2Icon } from "lucide-react";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
 
 const AuthHeader = () => {
   return (
@@ -9,6 +11,26 @@ const AuthHeader = () => {
                     Scribe
                 </h1>
             </Link>
+            <div className="hidden md:flex">{/* SearchBar */}</div>
+        </div>
+
+        <div className="flex items-center gap-8">
+            <Link href="/search" className="flex md:hidden">
+                <SearchIcon className="size-6 text-muted-foreground"/>
+            </Link>
+
+            <Link href="/new-post" className="flex items-center space-x-2 text-muted-foreground hover:text-black transition-colors">
+                <EditIcon className="size-5"/>
+                <span>Write</span>
+            </Link>
+
+            <ClerkLoading>
+                <Loader2Icon className="size-8 animate-spin"/>
+            </ClerkLoading>
+
+            <ClerkLoaded>
+                <UserButton/>
+            </ClerkLoaded>
         </div>
     </header>
   )
