acd986cf38b009a59022d572a83b1cb22e4f00f9
Richard Abishai <rickoshade1891@gmail.com>
2025-03-08 19:29:36 +0530

    Save new user data upon authentication in Home component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,13 @@
 import LandingPage from "@/components/LandingPage";
 import { currentUser } from "@clerk/nextjs/server";
+import { saveNewUser } from "@/services/userServices";
 
 export default async function Home() {
     const  user  = await currentUser();
 
     if (!user) return <LandingPage/>;
+
+    await saveNewUser(user);
     return (
         <div className="container">
             Hello
