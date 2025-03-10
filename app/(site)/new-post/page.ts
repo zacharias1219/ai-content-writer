e9099a9df28c534b22ddba5d07454c832418408e
Richard Abishai <rickoshade1891@gmail.com>
2025-03-10 19:32:52 +0530

    Add PostGenerator component and Textarea UI component for new post creation

--- app/(site)/new-post/page.ts
+++ app/(site)/new-post/page.ts
@@ -1,5 +1,7 @@
+import PostGenerator from "@/components/PostGenerator";
+
 export default async function NewPostPage() {
     return (
-        <PostGenerator/>
+        <PostGenerator/>        
     )
 }
---
+++ components/PostGenerator.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+const PostGenerator = () => {
+  return (
+    <div>
+        <form className="relative">
+            <div className="h-40 rounded-2xl p-0.5 bg-gradient-to-br from-purple-500 to-pink-500">
+                <div className="h-full w-full rounded-[13.5px] bg-gray-50">
+                    
+                </div>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default PostGenerator;
