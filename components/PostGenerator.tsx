f6befe4cd03f836d139004ac698116614a35dd52
Richard Abishai <rickoshade1891@gmail.com>
2025-03-10 19:35:44 +0530

    Add Textarea component to PostGenerator and create Input component for form handling

--- components/PostGenerator.tsx
+++ components/PostGenerator.tsx
@@ -1,12 +1,19 @@
 "use client";
 
+import { Textarea } from "./ui/textarea";
+
 const PostGenerator = () => {
   return (
     <div>
         <form className="relative">
             <div className="h-40 rounded-2xl p-0.5 bg-gradient-to-br from-purple-500 to-pink-500">
                 <div className="h-full w-full rounded-[13.5px] bg-gray-50">
-                    
+                    <Textarea
+                    name="prompt"
+                    maxLength={500}
+                    placeholder="Describe a topic youu want to write about"
+                    className="h-full text-sm md:text-base shadow-none border-none focus-visible:ring-0 focus-visible:outline-none resize-none lg:overflow-hidden"
+                    />
                 </div>
             </div>
         </form>
